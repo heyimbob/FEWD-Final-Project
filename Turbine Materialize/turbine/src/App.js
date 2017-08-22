887aa01c456a2624bc200af2119680649fbf33a1
Bob <29528303+wearytorch@users.noreply.github.com>
2017-08-21 22:22:40 -0500

    Added React

--- Turbine Materialize/turbine/src/App.js
+++ Turbine Materialize/turbine/src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Quiz from './quiz'; 
+import Trainer from './Trainer'
 
 class App extends Component {
+
+ // this.state....
+
   render() {
+
+
+  var trainer = <h1>trainer</h1>;
+
+  if(4 > 3){
+    trainer = <p>fgfdg</p>
+  }
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+        {trainer}
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Quiz onChange={console.log}/>
         </p>
       </div>
     );
